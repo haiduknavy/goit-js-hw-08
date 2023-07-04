4d8fe6272a57320b93abd8730e070b81e1dc5f84
haiduknavy <79574457+haiduknavy@users.noreply.github.com>
2023-07-04 13:48:48 +0000

    Deploying to gh-pages from @ haiduknavy/goit-js-hw-08@c304db7d7fbf02bb9e67b5f91d23a03212df3ac6 🚀

---
+++ 01-gallery.870b502a.js
@@ -0,0 +1,2 @@
+!function(){console.log(3242);console.log(5)}();
+//# sourceMappingURL=01-gallery.870b502a.js.map
---
+++ 01-gallery.9b12746f.js
@@ -0,0 +1,2 @@
+console.log(3242);console.log(5);
+//# sourceMappingURL=01-gallery.9b12746f.js.map
